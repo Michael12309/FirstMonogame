abbd82f48ddf6ffbbc8bae3d357e0f04a026112b
Michael <michaelleonard5555@gmail.com>
2023-09-27 17:58:03 -0700

    basic code (player movement)

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,11 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    Texture2D playerTexture;
+    Vector2 playerPosition;
+    float playerSpeed;
+    bool flipPlayer;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -20,6 +25,11 @@ public class Game1 : Game
     {
         // TODO: Add your initialization logic here
 
+        playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                                    _graphics.PreferredBackBufferHeight / 2);
+
+        playerSpeed = .5F;
+        flipPlayer = false;
         base.Initialize();
     }
 
@@ -28,6 +38,7 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        playerTexture = Content.Load<Texture2D>("The_Knight_Idle");
     }
 
     protected override void Update(GameTime gameTime)
@@ -36,6 +47,19 @@ public class Game1 : Game
             Exit();
 
         // TODO: Add your update logic here
+        KeyboardState kstate = Keyboard.GetState();
+
+        if (kstate.IsKeyDown(Keys.A))
+        {
+            playerPosition.X -= playerSpeed * (float)gameTime.ElapsedGameTime.Milliseconds;
+            flipPlayer = true;
+        }
+
+        if (kstate.IsKeyDown(Keys.D))
+        {
+            playerPosition.X += playerSpeed * (float)gameTime.ElapsedGameTime.Milliseconds;
+            flipPlayer = false;
+        }
 
         base.Update(gameTime);
     }
@@ -45,6 +69,18 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.ForestGreen);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(
+            playerTexture,
+            playerPosition,
+            null,
+            Color.White,
+            0f,
+            new Vector2(playerTexture.Width / 2, playerTexture.Height / 2),
+            Vector2.One,
+            flipPlayer ? SpriteEffects.FlipHorizontally : SpriteEffects.None,
+            0f);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
